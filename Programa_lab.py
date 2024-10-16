34b7043f20697a02788655c34ac54945637ac4ad
Ana Patiño <anispc3004@gmail.com>
2024-10-16 12:59:45 -0500

    cambiosmenu

--- Programa_lab.py
+++ Programa_lab.py
@@ -1,24 +1,5 @@
 #Funciones-----------------------------------------------------------------------------------------♥
 
-#F1-Menu_Principal
-def menu(): 
-    print("Bienvenido al programa de control de requisitos del laboratorio clinico")
-    while(True): #Hace que se repita el menú hasta que demos la opción de salir del programa.
-        n=int(input("""Ingrese la opción que desea realizar el día de hoy: 
-            ➜ 1. Realizar ensayos de control de requisitos.
-            ➜ 2. Realizar informes. 
-            ➜ 3. Ninguna de las anteriores.\nIngrese la opción aquí: """))
-        print("………………………………………………………………………………………………………………………………………………………………………………………………………………♥")
-        if (n==1):
-            menu_ensayos() #llama al subprograma que tiene las opciones de ensayos.
-        elif (n==2):
-            menu_informes()
-        elif(n==3):
-            print("Muchas gracias por usar nuestros servicios, vuelva prontro")
-            break 
-        else:
-            print("Ha seleccionado una opción que no existe en el menú. Intente de nuevo.")
-
 #F2-SubMenu_Ensayos
 def menu_ensayos():
     n=int(input("""En que sección del laboratorio desea realizar los ensayos
@@ -97,5 +78,27 @@ def hematologia():
 def menu_informes():
     print("aun no esta hecho pero no quiero que quede por error")
 
+#ALARMAS CONTROLADOR 
+def alarma_controles():
+    valor_control=int(input("Ingrese el valor que indica la maquina sobre los controles: "))
+
+
+#ALARMA CALIBRADOR
+
 #MAIN
-menu() #llamamos al menú principal
+print("Bienvenido al programa de control de requisitos del laboratorio clinico")
+while(True): #Hace que se repita el menú hasta que demos la opción de salir del programa.
+    n=int(input("""Ingrese la opción que desea realizar el día de hoy: 
+        ➜ 1. Realizar ensayos de control de requisitos.
+        ➜ 2. Realizar informes. 
+        ➜ 3. Ninguna de las anteriores.\nIngrese la opción aquí: """))
+    print("………………………………………………………………………………………………………………………………………………………………………………………………………………♥")
+    if (n==1):
+        menu_ensayos() #llama al subprograma que tiene las opciones de ensayos.
+    elif (n==2):
+         menu_informes()
+    elif(n==3):
+        print("Muchas gracias por usar nuestros servicios, vuelva prontro")
+        break 
+    else:
+        print("Ha seleccionado una opción que no existe en el menú. Intente de nuevo.")
